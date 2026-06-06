40a4c2e27647a78b83da325081903fc6e0a2744a
Baymaxxxu <411212105@mahasiswa.undira.ac.id>
2026-06-07 01:23:49 +0700

    perbaiki fitur edit, hapus dan detail

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,28 @@ use App\Http\Controllers\ReportController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::resource('kategori', CategoryController::class)->names('categories');
-Route::resource('barang', ItemController::class)->names('items');
-Route::resource('donatur', DonorController::class)->names('donors');
-Route::resource('penerima', RecipientController::class)->names('recipients');
+Route::resource('kategori', CategoryController::class)
+    ->names('categories')
+    ->parameters(['kategori' => 'category']);
+
+Route::resource('barang', ItemController::class)
+    ->names('items')
+    ->parameters(['barang' => 'item']);
+
+Route::resource('donatur', DonorController::class)
+    ->names('donors')
+    ->parameters(['donatur' => 'donor']);
+
+Route::resource('penerima', RecipientController::class)
+    ->names('recipients')
+    ->parameters(['penerima' => 'recipient']);
 
 Route::resource('bantuan-masuk', IncomingDonationController::class)
-    ->names('incoming-donations');
+    ->names('incoming-donations')
+    ->parameters(['bantuan-masuk' => 'incomingDonation']);
 
 Route::resource('distribusi', OutgoingDistributionController::class)
-    ->names('outgoing-distributions');
+    ->names('outgoing-distributions')
+    ->parameters(['distribusi' => 'outgoingDistribution']);
 
 Route::get('/laporan', [ReportController::class, 'index'])->name('reports.index');
